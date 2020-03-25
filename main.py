168fd8004fc38dfb7c01b0356ff743ff0cd12334
Sonosive <galeant222@gmail.com>
2020-03-24 18:44:36 -0700

    Updated discordpy to 1.3.2'

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import discord
+import json
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='c!')
+
+
+@bot.event
+async def on_ready():
+    game = discord.Game("c!info or c!help")
+    await bot.change_presence(
+        status=discord.Status.online, activity=game)
+    print(f"Logged in as {bot.user.name} - {bot.user.id}\n"
+          f"Version: {discord.__version__}")
+
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+bot.run(data["token"], bot=True)
